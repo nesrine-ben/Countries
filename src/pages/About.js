fe1c15bb8f3bd56293d20b75048f1445710bed44
nesssrina2019 <nesssrina2019@gmail.com>
2022-08-02 19:17:58 +0100

    first commit

---
+++ src/pages/About.js
@@ -0,0 +1,19 @@
+import React from 'react';
+import Logo from '../components/Logo';
+import Navigation from '../components/Navigation';
+
+const About = () => {
+    return (
+        <div>
+            <Logo />
+            <Navigation />
+            <h1>A propos</h1>
+            <br/>
+            <p>Lorem ipsum dolor sit amet consectetur adipisicing elit.</p>
+            <br/>
+            <p>Lorem ipsum dolor, </p>
+        </div>
+    );
+};
+
+export default About;
